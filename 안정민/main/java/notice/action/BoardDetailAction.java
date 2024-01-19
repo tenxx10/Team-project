ce1c048ba9dc481cb72144c3c7cd4f1060736d6e
dkslanjgkfkrh <155530086+dkslanjgkfkrh@users.noreply.github.com>
2024-01-19 17:04:49 +0900

    Add files via upload

---
+++ 안정민/main/java/notice/action/BoardDetailAction.java
@@ -0,0 +1,25 @@
+package notice.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import notice.svc.BoardDetailService;
+import notice.model.ActionForward;
+import notice.model.BoardBean;
+
+ public class BoardDetailAction implements Action {
+	 
+	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{ 
+	   	
+		int board_num=Integer.parseInt(request.getParameter("board_num"));
+		String page = request.getParameter("page");
+		BoardDetailService boardDetailService = new BoardDetailService();
+		BoardBean article = boardDetailService.getArticle(board_num);
+		ActionForward forward = new ActionForward();
+		request.setAttribute("page", page);
+	   	request.setAttribute("article", article);
+   		forward.setPath("view/board/noticeview.jsp");
+   		return forward;
+
+	 }
+	 
+}
---
+++ 안정민/main/java/notice/action/BoardListAction.java
@@ -0,0 +1,49 @@
+package notice.action;
+
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import notice.svc.BoardListService;
+import notice.model.ActionForward;
+import notice.model.BoardBean;
+import notice.model.PageInfo;
+
+ public class BoardListAction implements Action {
+	 
+	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
+		 
+		ArrayList<BoardBean> articleList=new ArrayList<BoardBean>();
+	  	int page=1;
+		int limit=10;
+		
+		if(request.getParameter("page")!=null){
+			page=Integer.parseInt(request.getParameter("page"));
+		}
+		
+		BoardListService boardListService = new BoardListService();
+		int listCount=boardListService.getListCount(); //�� ����Ʈ ���� �޾ƿ�.
+		articleList = boardListService.getArticleList(page,limit); //����Ʈ�� �޾ƿ�.
+		//�� ������ ��.
+   		int maxPage=(int)((double)listCount/limit+0.95); //0.95�� ���ؼ� �ø� ó��.
+   		//���� �������� ������ ���� ������ ��(1, 11, 21 ��...)
+   		int startPage = (((int) ((double)page / 10 + 0.9)) - 1) * 10 + 1;
+   		//���� �������� ������ ������ ������ ��.(10, 20, 30 ��...)
+   	        int endPage = startPage+10-1;
+
+   		if (endPage> maxPage) endPage= maxPage;
+
+   		PageInfo pageInfo = new PageInfo();
+   		pageInfo.setEndPage(endPage);
+   		pageInfo.setListCount(listCount);
+		pageInfo.setMaxPage(maxPage);
+		pageInfo.setPage(page);
+		pageInfo.setStartPage(startPage);	
+		request.setAttribute("pageInfo", pageInfo);
+		request.setAttribute("articleList", articleList);
+		ActionForward forward= new ActionForward();
+   		forward.setPath("view/board/noticelist.jsp");
+   		return forward;
+   		
+	 }
+	 
+ }
