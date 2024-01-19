5abb690565c2b41743c8e93a6538c4828d6e7ee3
dkslanjgkfkrh <155530086+dkslanjgkfkrh@users.noreply.github.com>
2024-01-19 11:06:56 +0900

    Delete 안정민 directory

--- 안정민/main/java/notice/action_book/Action.java
+++
@@ -1,8 +0,0 @@
-package notice.action_book;
-
-import javax.servlet.http.*;
-import vaccine.model_book.ActionForward;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
