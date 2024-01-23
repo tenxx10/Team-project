02e125ac85a088429ea3201568b933bbe57b32d1
dkslanjgkfkrh <155530086+dkslanjgkfkrh@users.noreply.github.com>
2024-01-23 16:03:06 +0900

    Delete 안정민/main directory

--- 안정민/main/java/notice/action/Action.java
+++
@@ -1,8 +0,0 @@
-package notice.action;
-
-import javax.servlet.http.*;
-import notice.model.ActionForward;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
