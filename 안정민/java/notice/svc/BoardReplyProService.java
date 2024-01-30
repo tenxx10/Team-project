765e8a0755b9428ad6a5cda963083efb5aa4af8f
dkslanjgkfkrh <155530086+dkslanjgkfkrh@users.noreply.github.com>
2024-01-30 11:17:50 +0900

    Add files via upload

---
+++ 안정민/java/notice/svc/BoardReplyProService.java
@@ -0,0 +1,33 @@
+package notice.svc;
+
+import static notice.action.JdbcUtil.*;
+import java.sql.Connection;
+import notice.action.BoardDAO;
+import notice.model.BoardBean;
+
+public class BoardReplyProService {
+
+	public boolean replyArticle(BoardBean article) throws Exception{
+		// TODO Auto-generated method stub
+		
+		boolean isReplySuccess = false;
+		int insertCount = 0;
+		Connection con = getConnection();
+		BoardDAO boardDAO = BoardDAO.getInstance();
+		boardDAO.setConnection(con);
+		insertCount = boardDAO.insertReplyArticle(article);
+		
+		if(insertCount > 0){
+			commit(con);
+			isReplySuccess = true;
+		}
+		else{
+			rollback(con);
+		}
+		
+		close(con);
+		return isReplySuccess;
+		
+	}
+
+}
