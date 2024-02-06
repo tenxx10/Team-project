070fa9eda0819a6df754fdfbb7566bad8833d178
Jay lee <143534556+tenxx10@users.noreply.github.com>
2024-02-06 13:11:43 +0900

    Delete 3차 directory

--- 3차/java/board/action/Action2.java
+++
@@ -1,8 +0,0 @@
-package board.action;
-
-import javax.servlet.http.*;
-import board.model.ActionForward2;
-
-public interface Action2{
-	public ActionForward2 execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
